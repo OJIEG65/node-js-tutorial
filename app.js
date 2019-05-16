20e421e55d9f725258a7948c798541cb3f524911
OJIEG <olo@keepit.com>
2019-05-16 19:34:57 +0300

    3.35

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    const method = req.method;
+
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title> enter message </title></head>');
+        res.write('<body>' +
+            '<form action="/message" method="POST"> ' +
+            '<input type="text" name="message">' +
+            '<button type="submit"> send</button><' +
+            '/form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk)
+        });
+
+        return req.on('end', () => {
+            const parserBody = Buffer.concat(body).toString();
+            const message = parserBody.split('=')[1];
+            fs.writeFile('message.txt', message, (error) => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end()
+            });
+        });
+
+    }
+
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title> Me fisrt page </title></head>');
