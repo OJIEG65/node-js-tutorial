fa1b125a1050fa2acc0f40db0a0303d9b25e8da3
OJIEG <olo@keepit.com>
2019-06-15 10:23:30 +0300

    210

--- util/database.js
+++ util/database.js
@@ -1,8 +1,27 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+let _db;
+const mongoConnect = (callback) => {
+    MongoClient
+        .connect('mongodb+srv://OJIEG65:JVrNAGV32MIC37el@nodejsexpress-yxrr9.mongodb.net/shop?retryWrites=true&w=majority')
+        .then(client => {
+            console.log('connected');
+            _db = client.db();
+            callback()
+        })
+        .catch(e => {
+            console.log(e);
+            throw e;
+        });
+};
 
-module.exports = sequelize;
+const getDB =()=>{
+    if(_db){
+        return _db
+    }
+    throw 'No database found!!!!'
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDB = getDB;
